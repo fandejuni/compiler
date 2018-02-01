4019e5e1825fcea26ee3c8b3ee8803d6a40dc315
fandejuni <t.dardinier@gmail.com>
2018-02-01 17:03:16 +0100

    Structure OK

--- typing.ml
+++ typing.ml
@@ -11,4 +11,33 @@ let string_of_type = function
   | Ttypenull  -> "typenull"
 
 let program p =
-  assert false (* À MODIFIER *)
+    assert("Failure")
+
+let rec gamma_mem x = function
+    | [] -> false
+    | t::q -> t.str_name = x || gamma_mem x q
+
+let check_type gamma = function
+    | Tint -> true
+    | Tstructp(ident) -> gamma_mem ident gamma
+
+(* gamma: structure list *)
+
+(* TODO: check si deux identifiants égaux *)
+let rec struct_bien_formee gamma = function
+    | [] -> true
+    | (typ, ident)::q -> check_type gamma typ && struct_bien_formee gamma q
+
+let check_function decl_fun gamma =
+    let b1 = check_type gamma decl_fun.typ in
+    let b2 = 
+    
+
+let jugement gamma = function
+    | Dstruct((ident, decl_list)) -> if (struct_bien_formee gamma decl_list) && (not (gamma_mem ident gamma)) then
+        (ident, decl_list)::gamma
+    else
+        raise Error("Structure mal formée ou type existant")
+    | Dfun(decl_fun) -> if (check_function decl_fun gamma) then ...
+
+
