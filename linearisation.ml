4cf19bb9abff70157fee3096bb4b8b1a6b367004
fandejuni <t.dardinier@gmail.com>
2018-03-17 20:51:23 +0100

    Why not

--- linearisation.ml
+++ linearisation.ml
@@ -36,9 +36,7 @@ let operand (op: Ltltree.operand) =
     | Reg(r) -> reg (register(r))
     | Spilled(i) -> ind ~ofs:i X86_64.rbp 
 
-let convert_label (l: Label.t) =
-    match (l :> string) with
-    | x -> x
+let convert_label (l: Label.t) = (l :> string)
 
 let rec lin g l =
   if not (Hashtbl.mem visited l) then begin
@@ -52,19 +50,89 @@ let rec lin g l =
 and instr g l = function
     | Ltltree.Econst (n, r, l1) ->
         emit l (movq (imm32 n) (operand r)); lin g l1
-    | Ltltree.Eload(r1, i, r2, label) -> raise (Error "TODO")
-    | Ltltree.Estore(r1, r2, i, label) -> raise (Error "TODO")
+    | Ltltree.Eload(r1, i, r2, label) ->
+        emit l (movq (ind ~ofs:i (register r1)) (operand (Reg(r2))));
+        lin g label
+    | Ltltree.Estore(r1, r2, i, label) ->
+        emit l (movq (operand (Reg(r1))) (ind ~ofs:i (register r2)));
+        lin g label
     | Ltltree.Egoto(label) ->
-            emit l (jmp (convert_label label)); lin g label
+        emit l (jmp (convert_label label)); lin g label
     | Ltltree.Ereturn ->
-            emit l ret
-    | Ltltree.Emunop(munop, operand, label) -> raise (Error "TODO")
-    | Ltltree.Embinop(mbinop, o1, o2, label) -> raise (Error "TODO")
-    | Ltltree.Emubranch(mubranch, operand, l1, l2) -> raise (Error "TODO")
-    | Ltltree.Embbranch(mbbranch, o1, o2, l1, l2) -> raise (Error "TODO")
-    | Ltltree.Epush(operand, label) -> raise (Error "TODO")
-    | Ltltree.Ecall(ident, label) -> raise (Error "TODO")
-    | Ltltree.Epop(register, label) -> raise (Error "TODO")
+        emit l ret
+    | Ltltree.Emunop(munop, r, label) ->
+        let i =
+        begin match munop with
+        | Maddi(n) -> addq (imm32 n) (operand r)
+        | Msetei(n) -> raise (Error "TODO")
+        | Msetnei(n) -> raise (Error "TODO")
+        end in
+        emit l i; lin g label
+    | Ltltree.Embinop(mbinop, r1, r2, label) ->
+        let o1 = operand r1 in
+        let o2 = operand r2 in
+        let i =
+        begin match mbinop with
+		| Mmov -> movq o1 o2
+		| Madd -> addq o1 o2
+		| Msub -> subq o1 o2
+		| Mmul -> imulq o1 o2
+		| Mdiv -> raise (Error "TODO")
+		| Msete -> raise (Error "TODO")
+		| Msetne -> raise (Error "TODO")
+		| Msetl -> raise (Error "TODO")
+		| Msetle -> raise (Error "TODO")
+		| Msetg -> raise (Error "TODO")
+		| Msetge -> raise (Error "TODO")
+        end in
+        emit l i; lin g label
+    | Ltltree.Emubranch(mubranch, r, l1, l2) ->
+        begin match mubranch with
+        | Mjz ->
+            begin
+                emit l (jz (convert_label l1))
+            end
+		| Mjnz -> raise (Error "TODO")
+		| Mjlei(n) -> raise (Error "TODO")
+		| Mjgi(n) -> raise (Error "TODO")
+        ;
+        let l2 = Label.fresh () in
+        emit l2 (jmp (convert_label l2));
+        lin g l2; lin g l1;
+        raise (Error "TODO")
+		end
+    | Ltltree.Embbranch(mbbranch, o1, o2, l1, l2) ->
+		begin
+        match mbbranch with
+        | Mjl -> emit l (jl (convert_label l1))
+		| Mjle -> emit l (jle (convert_label l1))
+        ;
+        let l2 = Label.fresh () in
+        emit l2 (jmp (convert_label l2));
+        lin g l2; lin g l1
+        end
+    | Ltltree.Epush(r, label) ->
+        emit l (pushq (operand r)); lin g label
+    | Ltltree.Ecall(ident, label) ->
+        emit l (call ident); lin g label
+    | Ltltree.Epop(r, label) ->
+        emit l (popq (register r)); lin g label
 
 let program (f: Ltltree.file) =
-    raise (Error "Not yet implemented")
+    let aux (deffun: Ltltree.deffun) =
+        emit_wl (label (deffun.fun_name));
+        lin (deffun.fun_body) (deffun.fun_entry)
+    in
+    List.iter aux (f.funs);
+    let text = ref (globl "main") in
+    let treat_code = function
+    | Code(c) -> text := c ++ (!text)
+    | Label(l) ->
+        if Hashtbl.mem labels l then text := (label (convert_label l)) ++ (!text)
+    in
+    List.iter treat_code (!code);
+    let p = {
+        text = !text;
+        data = nop;
+    } in
+    print_program std_formatter p
